cb22d7bc64a3d307794eeaaeb2ca3edae34bbd26
mmjun <imikejun@gmail.com>
2018-01-09 23:52:02 -0800

    updated js code

--- js/madlib-console.js
+++ js/madlib-console.js
@@ -1,14 +1,22 @@
 var startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
 var startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
+var savedstartup = []
 
 var random1 = Math.floor((Math.random() * startupX.length));
 var random2 = Math.floor((Math.random() * startupY.length));
 
-document.getElementById('create').onclick = madlib;
+document.getElementById('create').onclick = createStartup;
 
-function madlib () {
+function createStartup () {
   document.getElementById('xForY').innerHTML = 'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2];
 }
+
+document.getElementById('save').onclick = saveStartup;
+
+function saveStartup () {
+
+}
+
 
 
 console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
